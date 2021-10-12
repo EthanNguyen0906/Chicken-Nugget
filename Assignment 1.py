675fd72f943304a8d8cda514ff3066b4023c6544
Ethan Nguyen <979800@learn.vsb.bc.ca>
2021-10-12 14:36:05 -0700

    Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,14 @@
+import random
+
+Tyrannosaurus = ["What is your favourite colour?", "What is your favourite dinosaur?", "Where do u live?", "Credit Card Info?", "How many fingers do u have?", "Do you like Chicken Nuggets?"]
+print(input(random.choice(Tyrannosaurus)))
+Velociraptor = ["Why?", "You're weird.", "Pretty cool.","Same","Rlly?"]            
+print(input(random.choice(Velociraptor)))
+Stegosaurus = ["What is your favourite weather?", "How old are you?", "Wut kind of chair do u prefer?"]
+print(input(random.choice(Stegosaurus)))
+Pterodactyl = ["Oh, nice.", "Ok", "O_O","Epic","Same"]
+print(input(random.choice(Pterodactyl)))
+Brachiosaurus = ["What is your favourite shade of white?", "Where do you like to get ice cream?","Your favourite dinosaur?"]
+print(input(random.choice(Brachiosaurus)))
+Triceratops = ["Oh cool!", "That's nice.", "titanosaur Argentinosaurus huinculensis", "Alright, Bye Bye","Okay, Cya!"]
+print(input(random.choice(Triceratops)))
